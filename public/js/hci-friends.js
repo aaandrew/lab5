a953319989500f7e554f480d7527ce1238d13a97
Andrew Du <aadu@ucsd.edu>
2016-02-04 00:48:55 -0800

    lab5

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -3,6 +3,12 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+
+	$(".friend").click(function(){
+		var name = $(this).text();
+		$(this).text(anagrammedName(name));
+	});
+
 })
 
 /*
